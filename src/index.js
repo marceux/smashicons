c57b5295969767c4f7a8712b8b52930e69f55eb1
Marco Salazar de Leon <msalazardeleon@gmail.com>
2016-06-22 23:34:42 -0700

    Fixed poorly named function and got it working

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function StockIcon(props) {
+  return (
+    <div className="stock-icon">
+      <p>Trying this again</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <StockIcon />,
+  document.getElementById('root')
+);
