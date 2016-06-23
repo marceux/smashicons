9016bd0d909875438998d80b7d4d239abf5f0d23
Marco Salazar de Leon <msalazardeleon@gmail.com>
2016-06-23 00:07:16 -0700

    Created loading svg example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-function StockIcon(props) {
-  return (
-    <div className="stock-icon">
-      <p>Trying this again</p>
-    </div>
-  );
+let SheikNormal = require('./assets/svg/sheik/sheik-normal.svg');
+
+class StockIcon extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementById('sheik').innerHTML = SheikNormal;
+  }
+
+  render () {
+    return (
+      <div id="sheik"></div>
+    );
+  }
 }
 
 ReactDOM.render(
